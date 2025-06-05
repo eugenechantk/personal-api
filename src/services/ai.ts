9ee2bd9fddaf14a11fe5de56e8576f4742b9640e
eugenechantk <me@eugenechantk.me>
2025-06-06 04:36:07 +0800

    use faster 1.5 flash model + improve prompt to make it more concise

--- src/services/ai.ts
+++ src/services/ai.ts
@@ -10,8 +10,11 @@ export class AIService {
   private model: any;
 
   constructor(apiKey: string) {
-    this.model = google("gemini-2.5-flash-preview-04-17", {
+    this.model = google("gemini-1.5-flash", {
       useSearchGrounding: true,
+      dynamicRetrievalConfig: {
+        mode: "MODE_UNSPECIFIED",
+      },
     });
   }
 
@@ -29,7 +32,13 @@ export class AIService {
     }
 
     const result = await generateText({
-      model: google("gemini-1.5-flash"),
+      model: this.model,
+      system: `
+      You will mostly be answering questions. 
+      1. Keep your answers concise and precise. Preferrably in 1-2 sentences. When necessary, use bullet points.
+      2. You will be given a prompt and sometimes an image. Use the image to help you answer the question if it is provided.
+      3. the output should be in plain text. So don't use any markdown.
+      `,
       messages: [
         {
           role: "user",
@@ -41,6 +50,8 @@ export class AIService {
       ],
     });
     const { text, sources } = result;
+
+    console.log(result);
 
     return { text, sources };
   }
